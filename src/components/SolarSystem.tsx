69bfdabe8234674b5234f4ff24ea82f354ab6a1e
Alexandre Carvalho <alex380bb@gmail.com>
2023-07-12 19:39:42 -0300

    requesito 3,4

---
+++ src/components/SolarSystem.tsx
@@ -0,0 +1,9 @@
+import Title from "./Title";
+
+function SolarSystem() {
+    return <div data-testid="solar-system">
+      <Title headline="Planetas" />
+    </div>;
+};
+
+export default SolarSystem;
